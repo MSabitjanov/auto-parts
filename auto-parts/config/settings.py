e28aacb6aa60de4e30c490d1114962290da3cd04
Muzaffar Sabitjanov <msabitjanov1@gmail.com>
2023-10-21 18:08:43 +0500

    switch to default db for locale

--- auto-parts/config/settings.py
+++ auto-parts/config/settings.py
@@ -104,12 +104,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": config("POSTGRES_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
 
