370ca0540f377ae2d56782973cd60eb6cafe1931
Muzaffar <msabitjanov1@gmail.com>
2023-10-25 17:49:32 +0500

    revrite the chat view

--- auto-parts/apps/communication/api/views.py
+++ auto-parts/apps/communication/api/views.py
@@ -5,6 +5,8 @@ from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.communication.models import Messages, Chat
 
@@ -47,19 +49,30 @@ class ChatViewSet(ChatViewSet):
         get_object_or_404(User, id=participants[0])
         return participants + [self.request.user.id]
 
-    def check_uniqueness_of_chat_and_create(self, serializer, participants):
-        print(type(participants))
-        chat = Chat.objects.filter(participants__in=participants).distinct()
+    def check_uniqueness_of_chat(self, serializer, participants):
+        chat = Chat.objects.filter(participants=participants[0]).filter(participants=participants[1]).filter(is_active=True)
 
         if chat.exists():
-            raise serializers.ValidationError("Chat with this user already exists")
-        else:
-            serializer.save(participants=participants)
+            return chat.first().id
 
     def perform_create(self, serializer):
         participants = self.validate_participants(serializer)
-        print(participants)
-        self.check_uniqueness_of_chat_and_create(serializer, participants)
+        self.check_uniqueness_of_chat(serializer, participants)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        participants = self.validate_participants(serializer)
+        chat_id = self.check_uniqueness_of_chat(serializer, participants)
+
+        if chat_id:
+            return Response({"chat_id": chat_id, "message": "Chat between these users already exists."},
+                            status=status.HTTP_200_OK)
+        else:
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class MessageViewSet(ListCreateAPIView):
