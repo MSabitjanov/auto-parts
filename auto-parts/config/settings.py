9f84150fadbc43d7d491a6f222dd52ac5fb27494
Muzaffar Sabitjanov <msabitjanov1@gmail.com>
2024-01-08 14:54:44 +0500

    Add localhost to allowerd hosts

--- auto-parts/config/settings.py
+++ auto-parts/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["localhost", "95.130.227.232"]
+ALLOWED_HOSTS = ["127.0.0.1", "95.130.227.232"]
 
 
 # Application definition
