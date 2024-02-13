552fdd9f8bbf89ef5ad22b495eb31a0782bddcad
Dilshodbek8 <20020905xd@gmail.com>
2024-02-13 17:09:13 +0500

    Correct static url

--- auto-parts/config/settings.py
+++ auto-parts/config/settings.py
@@ -163,7 +163,7 @@ MODELTRANSLATION_TRANSLATION_FILES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     # str(BASE_DIR / "static"),
